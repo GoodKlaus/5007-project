7ec1fd88b83cb7f1ccb23a0d797790786f1f8b2f
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-03-29 20:22:40 +0800

    Update Content.jsx

--- ui/src/Content.jsx
+++ ui/src/Content.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Switch, Route, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
+import Home from './Home.jsx';
+import Results from './Results.jsx';
+import InfoDetail from './Info.jsx';
+import Summary from './Summary.jsx';
 import AboutUs from './AboutUs.jsx';
 import Login from './Login.jsx';
 import Register from './Register.jsx';
@@ -8,10 +12,14 @@ import Profile from './Profile.jsx';
 
 const NotFound = () => <h1>Page Not Found</h1>;
 
-export default function Contents() {
+function Content() {
   return (
     <Switch>
-      {/* <Redirect exact from="/" to="/issues" /> */}
+      <Redirect exact from="/" to="/home" />
+      <Route path="/home" component={Home} />
+      <Route path="/results" component={Results} />
+      <Route path="/info" component={InfoDetail} />
+      <Route path="/summary" component={Summary} />
       <Route path="/aboutUs" component={AboutUs} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
@@ -20,3 +28,5 @@ export default function Contents() {
     </Switch>
   );
 }
+
+export default withRouter(Contents);
