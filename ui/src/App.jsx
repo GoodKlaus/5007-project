51ca176f1a143ce8a4815bf9d24b0ddce3885d52
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-03-29 19:58:25 +0800

    Update App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,8 +1,6 @@
-import 'babel-polyfill';
-import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Page from './Page.jsx';
 
@@ -13,7 +11,3 @@ const element = (
 );
 
 ReactDOM.render(element, document.getElementById('contents'));
-
-if (module.hot) {
-  module.hot.accept();
-}
