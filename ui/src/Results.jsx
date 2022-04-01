09e0361272f634912b18dd85105990ae132e2ea8
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-04-01 18:01:19 +0800

    Update Results.jsx

--- ui/src/Results.jsx
+++ ui/src/Results.jsx
@@ -12,7 +12,7 @@ function Searching() {
   let location = useLocation();
 
   return (
-    <h1>Charging at: {location.state.area}</h1>
+    <h2 style={{color: "#283292"}}>Charging at: {location.state.area}</h2>
   );
 }
 
@@ -24,10 +24,10 @@ function Result(props) {
   }
 
   return  (
-    <div class="result" onClick={handlePush}>
-          <div class="text_left"><p>Address: {props.result.address[props.ind]}</p></div>
-          <div class="text_right"><p>Distance: {props.result.distance[props.ind]}km</p></div>
-          <p class="vacant_time">Vacant Time: {props.result.vacancy[props.ind].map(t => <span>{t}</span>).reduce((prev, curr) => [prev, ', ', curr])}</p>
+    <div className="result" onClick={handlePush}>
+          <div className="text_left"><h4>Address: {props.result.address[props.ind]}</h4></div>
+          <div className="text_right"><h4>Distance: {props.result.distance[props.ind]}km</h4></div>
+          <h4 className="vacant_time">Vacant Time: {props.result.vacancy[props.ind].map(t => <span>{t}</span>).reduce((prev, curr) => [prev, ', ', curr])}</h4>
     </div>
   );
 }
@@ -43,10 +43,12 @@ export default class Results extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <div className='result_content'>
         <Searching />
         <Result result={this.state.result} ind={0}/>
         <Result result={this.state.result} ind={1}/>
         <Result result={this.state.result} ind={2}/>
+        </div>
       </React.Fragment>
     );
   }
