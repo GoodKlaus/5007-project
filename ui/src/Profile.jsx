674e1a2edbeabb980fb3a2b1d9dce06367a2fc2d
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-04-29 21:29:06 +0800

    Update Profile.jsx

--- ui/src/Profile.jsx
+++ ui/src/Profile.jsx
@@ -14,10 +14,10 @@ export default class Register extends React.Component {
         if(this.props.isLogined === false){
             return (
                 <React.Fragment>
-                    <h3 style={{color:"red"}}>Error! Have not logged in. </h3>
+                    <h3 style={{color:"red"}}>You have logged out. </h3>
                 </React.Fragment>
             );
-        }else{
+        }else if (this.props.LoginedUser.isOwnerOfEVCharger){
             return (
                 <React.Fragment>
                 <div className='profile_content'>
@@ -33,10 +33,9 @@ export default class Register extends React.Component {
                         <label>{this.props.LoginedUser.phoneNumber}</label>
                         <br/><br/>
 
-                        <label style={{width:"150px", textAlign:"left", display:"inline-block"}}>Is a Owner of EV </label>
-                        <input type="checkbox" checked={this.props.LoginedUser.isOwnerOfEV} style={{width:"15px",height:"15px"}}/>
+                        <label style={{width:"150px", textAlign:"left", display:"inline-block"}}>Is a Owner of EV Charger</label>
+                        <input type="checkbox" checked={this.props.LoginedUser.isOwnerOfEVCharger} style={{width:"15px",height:"15px"}}/>
                         <br/><br/>
-
                         <label style={{width:"150px", textAlign:"left", display:"inline-block"}}>Address of Pile: </label>
                         <label>{this.props.LoginedUser.addressOfPile}</label>
                         <br/><br/>
@@ -44,7 +43,27 @@ export default class Register extends React.Component {
                         <label>S$ {this.props.LoginedUser.price} /hour</label>
                         <br/><br/>
                         <label style={{width:"150px", textAlign:"left", display:"inline-block"}}>Available Time: </label>
-                        <label>{this.props.LoginedUser.availableTime} </label>
+                        <label>{this.props.LoginedUser.availableTime.map(t => <span>{t}</span>).reduce((prev, curr) => [prev, ', ', curr])} </label>
+                        <br/><br/>
+                    </form>
+                    <button className='logoutButtom' onClick={this.logout}>Logout</button>
+                </div>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                <div className='profile_content'>
+                <h2>Profile</h2>
+                    <form name="registerForm" onSubmit={this.handleSubmit}>
+                        <label style={{width:"150px", textAlign:"left", display:"inline-block"}}>Name:</label>
+                        <label>{this.props.LoginedUser.name}</label>
+                        <br/><br/>
+                        <label style={{width:"150px", textAlign:"left", display:"inline-block"}}>Email:</label>
+                        <label>{this.props.LoginedUser.email}</label>
+                        <br/><br/>
+                        <label style={{width:"150px", textAlign:"left", display:"inline-block"}}>Phone Number: </label>
+                        <label>{this.props.LoginedUser.phoneNumber}</label>
                         <br/><br/>
                     </form>
                     <button className='logoutButtom' onClick={this.logout}>Logout</button>
