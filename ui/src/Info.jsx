f883a4d165d82fa8de991bf548540500f8d24563
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-04-29 21:27:47 +0800

    Update Info.jsx

--- ui/src/Info.jsx
+++ ui/src/Info.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Form, FormGroup, Col, ControlLabel, FormControl, Button } from 'react-bootstrap';
 import {useLocation, useHistory} from 'react-router-dom'
 
-const tt = [["9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"],
-    ["11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"],
-    ["9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00"],
-    ["10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"],
-    ["9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "16:00", "16:30", "17:00", "17:30", "18:00"],
-    ["9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"], 
-    ["9:30", "10:00", "10:30", "11:00", "12:00", "12:30", "13:00", "13:30", "14:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"]]
+import graphQLFetch from './graphQLFetch.js'
 
-var time_select = "";
+const tt = [["9:30"],
+    ["9:30"],
+    ["9:30"],
+    ["9:30"],
+    ["9:30"],
+    ["9:30"], 
+    ["9:30"]];
+var ind_from_result = 0;
+
+
 
 class TimeTable extends React.Component {
     constructor() {
@@ -18,9 +22,13 @@ class TimeTable extends React.Component {
     }
 
     render() {
-        const findTrueIndex = (element) => element == true;
-        const ind = this.props.info.days.findIndex(findTrueIndex);
-        const timing = this.props.info.timetable[ind];
+        var timing = [];
+        if (this.props.users.length > 1) {
+            timing = this.props.users[ind_from_result].availableTimeTable[this.props.days_index];
+        } else {
+            timing = this.props.users[0].availableTimeTable[this.props.days_index];
+        }
+        
 
         return (
             <div className="container">
@@ -40,12 +48,13 @@ class TimeTable extends React.Component {
 function Detail(props) {
     let location = useLocation();
     let ind_curr = parseInt(location.state.ind_curr);
+    ind_from_result = ind_curr;
 
     return (
         <div className='detail'>
-            <div className="text_left"><h3>Address: {location.state.result.address[ind_curr]}</h3></div>
-            <div className="text_right"><h3>Distance: {location.state.result.distance[ind_curr]} km</h3></div>
-            <h3 className="price">Price: S$ {props.price}/hour</h3>
+            <div className="text_left"><h3>Address: {location.state.address}</h3></div>
+            <div className="text_right"><h3>Distance: {location.state.dist} km</h3></div>
+            <h3 className="price">Price: S$ {location.state.price}/hour</h3>
         </div>
     );
 }
@@ -54,32 +63,74 @@ function Submission(props) {
     let history = useHistory();
     let location = useLocation();
     let ind_curr = parseInt(location.state.ind_curr);
-    
+
+    if (props.time_sel != " ") {
+        const selected_time = props.time_sel;
+        const tt_selected = props.users[ind_curr].availableTimeTable[props.days_index];
+        const time_index = tt_selected.indexOf(selected_time);
+
+        var selectionOK = true;
+        var hours = [];
+        for (let i=0; i<props.duration+1; i++) {
+            if(time_index+i>=tt_selected.length){
+                selectionOK = false;
+                break;
+            }
+            const colon_ind = tt_selected[time_index+i].indexOf(':');
+            var temp_hour = parseInt(tt_selected[time_index+i].slice(0, colon_ind));
+            if (i>0 && temp_hour-1>hours[i-1]) {
+                    selectionOK = false;
+                    break;
+            }
+            hours.push(temp_hour);
+        }
+        console.log(selectionOK);
+        if(!selectionOK){
+            alert("Your selection either exceeds owner's available time range or overlap booked timing, please select again");
+            refresh(history, location);
+        }
+    }
+
     return (
         <Button type="submit" onClick={()=>props.handlePush(history, location, ind_curr)}>Submit</Button>
     );
 }
+
 function refresh(hist, loc) {
     hist.replace('/reload');
     setTimeout(()=>{
-        hist.replace({pathname:'/info', state:{result: loc.state.result, ind_curr: loc.state.ind_curr}});
+        hist.replace({pathname:'/info', state:{address: loc.state.address, price: loc.state.price, dist: loc.state.dist, ind_curr: loc.state.ind_curr}});
     })
 }
 
 export default class InfoDetail extends React.Component {
     constructor() {
         super();
-        this.state = {info: 
-            {timetable: tt, days: [true,false,false,false,false,false,false]},
+        this.state = {users: [{availableTimeTable: tt}], days: [true,false,false,false,false,false,false], days_index: 0,
             time_sel: " ",
             duration: 1,
-            price: 20,
-            date_sel: false
+            date_sel: false,
         };
         this.show = this.show.bind(this);
         this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handlePush = this.handlePush.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+    
+    async loadData() {
+        const query = `query {
+          userChargerList {
+            id addressOfPile price availableTimeDisplay availableTimeTable
+          }
+        }`;
+        const data = await graphQLFetch(query);
+        if (data.userChargerList.length > 0) {
+          this.setState({ users: data.userChargerList });
+        }
     }
 
     show(addition) {
@@ -91,8 +142,7 @@ export default class InfoDetail extends React.Component {
         const day_of_week = (now.getDay() + addition) % 7;
         days_init[day_of_week] = true;
         
-        const new_info = { ...this.state.info, days: days_init}
-        this.setState({info: new_info, date_sel: date});
+        this.setState({days: days_init, days_index: addition, date_sel: date});
     }
 
     handleClick(t) {
@@ -104,41 +154,49 @@ export default class InfoDetail extends React.Component {
     }
 
     handlePush(history, location, ind_curr) {
+        console.log("no prob");
         if (this.state.time_sel != " ") {
             var now = new Date();
             const date_now = (now.getDate()) + '/' + (now.getMonth()+1) + '/' + now.getFullYear();
             history.push({pathname: "/summary", state: {
-                address: location.state.result.address[ind_curr],
-                price: this.state.price,
+                address: this.state.users[ind_curr].addressOfPile,
+                price: this.state.users[ind_curr].price,
                 timing: this.state.time_sel,
                 duration: this.state.duration,
-                date: (!this.state.date_sel) ? date_now : this.state.date_sel
+                date: (!this.state.date_sel) ? date_now : this.state.date_sel,
+                index: ind_curr,
+                days_index: this.state.days_index,
             }});
         } else {
             alert("You MUST select a charging time");
-                refresh(history, location);
-            } 
+            refresh(history, location);
+        } 
     }
 
     render() {
-        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        var days = [];
         var now = new Date();
+        for (let i=0; i<7; i++) {
+            var temp = new Date(now.getTime() + i*24*60*60*1000);
+            const date = (temp.getDate()) + '/' + (temp.getMonth()+1);
+            days.push(date);
+        }
 
         return (
-            <div>
-            <Detail price={this.state.price}/>
+            <React.Fragment>
+            <Detail />
             <div id="nav_div">
             <h3 style={{textAlign:'center'}}>Vancant Timing</h3>
             <ul id="nav">
                 <li><a onClick={()=>this.show(0)}>Today</a></li>
-                <li><a onClick={()=>this.show(1)}>{days[(now.getDay()+1)%7]}</a></li>
-                <li><a onClick={()=>this.show(2)}>{days[(now.getDay()+2)%7]}</a></li>
-                <li><a onClick={()=>this.show(3)}>{days[(now.getDay()+3)%7]}</a></li>
-                <li><a onClick={()=>this.show(4)}>{days[(now.getDay()+4)%7]}</a></li>
-                <li><a onClick={()=>this.show(5)}>{days[(now.getDay()+5)%7]}</a></li>
-                <li><a onClick={()=>this.show(6)}>{days[(now.getDay()+6)%7]}</a></li>
+                <li><a onClick={()=>this.show(1)}>{days[1]}</a></li>
+                <li><a onClick={()=>this.show(2)}>{days[2]}</a></li>
+                <li><a onClick={()=>this.show(3)}>{days[3]}</a></li>
+                <li><a onClick={()=>this.show(4)}>{days[4]}</a></li>
+                <li><a onClick={()=>this.show(5)}>{days[5]}</a></li>
+                <li><a onClick={()=>this.show(6)}>{days[6]}</a></li>
             </ul>
-            <TimeTable info={this.state.info} handleClick={this.handleClick}/>
+            <TimeTable users={this.state.users} days_index={this.state.days_index} handleClick={this.handleClick}/>
             </div>
             <Form horizontal className='info_form'>
                 <FormGroup>
@@ -149,21 +207,22 @@ export default class InfoDetail extends React.Component {
                     <Col sm={4}><h4>Your Charging Duration:</h4></Col>
                     <Col sm={6}>
                     <FormControl componentClass="select" onChange={this.handleChange}>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
+                        <option value={1}>1 hour</option>
+                        <option value={2}>2 hours</option>
+                        <option value={3}>3 hours</option>
+                        <option value={4}>4 hours</option>
+                        <option value={5}>5 hours</option>
                     </FormControl>
                     </Col>
                 </FormGroup>
                 <FormGroup>
                     <Col smOffset={4} sm={6}>
-                        <Submission handlePush={this.handlePush}/>
+                        <Submission handlePush={this.handlePush} users={this.state.users} time_sel={this.state.time_sel} days_index={this.state.days_index} duration={this.state.duration}/>
                     </Col>
                 </FormGroup>
             </Form>
-            </div>
+            </React.Fragment>
         );
     }
 }
+
