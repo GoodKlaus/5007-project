edf414e22722114fc8d672f90d5838fb71103567
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-04-29 21:28:42 +0800

    Update Page.jsx

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -47,44 +47,89 @@ export default class Page extends React.Component {
     }
 
     this.loadUserInfo = this.loadUserInfo.bind(this);
+    this.loadUserInfoLogin = this.loadUserInfoLogin.bind(this);
     this.setLoginInfo = this.setLoginInfo.bind(this);
     this.setLoginStatus = this.setLoginStatus.bind(this);
     this.checkUserIsExisted = this.checkUserIsExisted.bind(this);
     this.clearLoginedUserInfo = this.clearLoginedUserInfo.bind(this);
     this.addNewUser = this.addNewUser.bind(this);
+    this.addNewUserCharger = this.addNewUserCharger.bind(this);
 
   }
 
   async checkUserIsExisted(userInput) {
     const query = `query getUser ($userInput:userLoginInput!){
         getUser(userInput:$userInput){
-          name email phoneNumber isOwnerOfEV addressOfPile price availableTime
+          name email phoneNumber isOwnerOfEVCharger
       }
     }`;
     const data = await graphQLFetch(query, { userInput });
     if(data.getUser){
       return true
     }else{
-      return false
+      const query1 = `query getUserCharger ($userInput:userLoginInput!){
+        getUserCharger (userInput:$userInput){
+          name email phoneNumber isOwnerOfEVCharger
+        }
+      }`;
+      const data1 = await graphQLFetch(query1, { userInput });
+      if (data1.getUserCharger) {
+        return true;
+      } else {
+        return false;
+      }
     }
   }
 
   //save user infomation
   async loadUserInfo(userInput) {
-    const query = `query getUser ($userInput:userLoginInput!){
+    var query;
+    if (userInput.isOwnerOfEVCharger) {
+      query = `query getUser ($userInput:userLoginInput!){
+        getUserCharger(userInput:$userInput){
+          name email phoneNumber isOwnerOfEVCharger addressOfPile price availableTime availableTimeTable
+        }
+      }`;
+      delete userInput['isOwnerOfEVCharger'];
+      const data = await graphQLFetch(query, { userInput });
+      this.setState({LoginedUser:data.getUserCharger})
+    } else {
+        query = `query getUser ($userInput:userLoginInput!){
+              getUser(userInput:$userInput){
+                name email phoneNumber isOwnerOfEVCharger
+            }
+        }`;
+        delete userInput['isOwnerOfEVCharger'];
+        const data = await graphQLFetch(query, { userInput });
+        this.setState({LoginedUser:data.getUser});
+    }
+  }
+
+  async loadUserInfoLogin(userInput) {
+    var query = `query getUser ($userInput:userLoginInput!){
         getUser(userInput:$userInput){
-          name email phoneNumber isOwnerOfEV addressOfPile price availableTime
-      }
+          name email phoneNumber isOwnerOfEVCharger
+        }
     }`;
     const data = await graphQLFetch(query, { userInput });
-    this.setState({LoginedUser:data.getUser})
+    if (data.getUser){
+      this.setState({LoginedUser:data.getUser});
+    } else {
+        query = `query getUserCharger ($userInput:userLoginInput!){
+              getUserCharger (userInput:$userInput){
+                name email phoneNumber isOwnerOfEVCharger addressOfPile price availableTime availableTimeTable
+            }
+        }`;
+        const data = await graphQLFetch(query, { userInput });
+        this.setState({LoginedUser:data.getUserCharger});
+    }
   }
 
   //add new user infomation
   async addNewUser(userInput) {
     const query = `mutation registerNewUser ($userInput:userRegisterInput!){
         registerNewUser(userInput:$userInput){
-        name email phoneNumber isOwnerOfEV addressOfPile price availableTime
+        name email phoneNumber isOwnerOfEVCharger
       }
     }`;
     const data = await graphQLFetch(query, { userInput });
@@ -95,11 +140,30 @@ export default class Page extends React.Component {
     }
   }
 
+  async addNewUserCharger(userChargerInput) {
+    const query = `mutation registerNewUserCharger ($userChargerInput:userChargerRegisterInput!){
+        registerNewUserCharger(userChargerInput:$userChargerInput){
+        name email phoneNumber isOwnerOfEVCharger addressOfPile
+      }
+    }`;
+    const data = await graphQLFetch(query, { userChargerInput });
+    if(!data){
+      return null;
+    }else{
+      return data.registerNewUserCharger;
+    }
+  }
 
   //save login information(email,phoneNumber,password)
   setLoginInfo(loginInfo){
     this.setState({loginInfo:loginInfo});
-    this.loadUserInfo(loginInfo);
+    console.log(loginInfo);
+    if ("isOwnerOfEVCharger" in loginInfo) {
+      this.loadUserInfo(loginInfo);
+    } else {
+      this.loadUserInfoLogin(loginInfo);
+    }
+    
   }
 
   setLoginStatus(status){
@@ -120,8 +184,8 @@ export default class Page extends React.Component {
 
         <div className = "content"><Content isLogined = {this.state.isLogined} LoginedUser = {this.state.LoginedUser}
               setLoginInfo = {this.setLoginInfo}  setLoginStatus = {this.setLoginStatus} 
-              checkUserIsExisted = {this.checkUserIsExisted} loadUserInfo = {this.loadUserInfo}
-              clearLoginedUserInfo = {this.clearLoginedUserInfo} addNewUser= {this.addNewUser}/></div>
+              checkUserIsExisted = {this.checkUserIsExisted} loadUserInfo = {this.loadUserInfo} loadUserInfoLogin={this.loadUserInfoLogin}
+              clearLoginedUserInfo = {this.clearLoginedUserInfo} addNewUser= {this.addNewUser} addNewUserCharger={this.addNewUserCharger}/></div>
       </div>
       <footer>
           <small><i>Copyright &copy; <strong>ChargingDoor</strong><br></br><a href="mailto:Charging@Door.com">Charging@Door</a>
