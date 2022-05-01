108ee90e189803e439533fbce791f371327d9157
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-05-01 22:28:00 +0800

    Update target.js

--- api/target.js
+++ api/target.js
@@ -1,12 +1,14 @@
 const { UserInputError } = require('apollo-server-express');
 const { getDb, getNextSequence } = require('./db.js');
 
+//query target location searched at Home page (last element from the array stored in database)
 async function result() {
   const db = getDb();
   const result_target = await db.collection('targets').find({}).toArray();
   return result_target[result_target.length-1];
 }
 
+//add target location from Home page
 async function add(_, { target }) {
   const db = getDb();
 
